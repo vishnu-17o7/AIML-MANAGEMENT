4f0bf77c55b475cf478359d00c3c0c9fdb169ef7
Vishnu <vishnu.aadhya@gmail.com>
2023-09-06 21:11:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const login = require('./login'); // Import the login file/module
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    login.authenticateUser(username, password, (err, isAuthenticated) => {
+      if (err) {
+        console.error('Error during authentication:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      if (isAuthenticated) {
+        res.sendFile(path.join(__dirname, 'public', 'home.html'));
+      } else {
+        res.send('Login failed');
+      }
+    });
+  });
+
+// Handle GET request for the registration page
+app.get('/registerpage', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+// Handle GET request for the login page
+app.get('/loginpage', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
